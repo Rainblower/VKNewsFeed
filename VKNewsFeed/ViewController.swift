675256cbb59ac0428d1a3f55314718ea9145553b
Mikhail Pogodin <m.s.pogodin@gmail.com>
2019-07-17 14:25:03 +0300

    Initial Commit

---
+++ VKNewsFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKNewsFeed
+//
+//  Created by WSR on 17/07/2019.
+//  Copyright © 2019 Rainblower. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
